d9c4dfa223eb1a64ba0423d90c19321ad642b7a9
SeanCheatham <smcheath@syr.edu>
2014-09-29 14:14:16 -0400

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Board b = new Board();
         b.search(80);
         System.out.println(b.toString());
         System.out.println(b.evaluate());
     }
 
-
+    // Tes
 }
